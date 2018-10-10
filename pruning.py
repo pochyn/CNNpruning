1e8a0d2d7b71b31e6f63c8d55eab1de46f080c72
Stas Pochynok <staspochyn@gmail.com>
2018-10-10 09:18:32 -0400

    assign variables

--- pruning.py
+++ pruning.py
@@ -26,7 +26,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 FLAGS = None
-SPARSITY = 99
 
 # main function to build cnn
 def deepnn(x):
@@ -41,105 +40,115 @@ def deepnn(x):
     dropout.
   """
   keep_prob = tf.placeholder(tf.float32)
-  x_image = tf.reshape(x, [-1, 784])
+  with tf.name_scope('reshape'):
+    x_image = tf.reshape(x, [-1, 784])
 
   # construction first layer of size 1000
   layer1 = 1000
-  W_conv1 = weight_variable([784, layer1], False)
-  A1 = tf.nn.relu(tf.matmul(x_image, W_conv1))
+  with tf.name_scope('layer1'):
+      W_conv1 = weight_variable([784, layer1])
+      A1 = tf.nn.relu(tf.matmul(x_image, W_conv1))
 
   # construction first layer of size 1000
   layer2 = 1000
-  W_conv2 = weight_variable([layer1, layer2], False)
-  A2 = tf.nn.relu(tf.matmul(A1, W_conv2))
+  with tf.name_scope('layer2'):
+      W_conv2 = weight_variable([layer1, layer2])
+      A2 = tf.nn.relu(tf.matmul(A1, W_conv2))
 
   # construction first layer of size 500
   layer3 = 500
-  W_conv3 = weight_variable([layer2, layer3], False)
-  A3 = tf.nn.relu(tf.matmul(A2, W_conv3))
+  with tf.name_scope('layer3'):
+      W_conv3 = weight_variable([layer2, layer3])
+      A3 = tf.nn.relu(tf.matmul(A2, W_conv3))
 
   # construction first layer of size 200
   layer4 = 200
-  W_conv4 = weight_variable([layer3, layer4], False)
-  A4 = tf.nn.relu(tf.matmul(A3, W_conv4))
+  with tf.name_scope('layer4'):
+      W_conv4 = weight_variable([layer3, layer4])
+      A4 = tf.nn.relu(tf.matmul(A3, W_conv4))
 
-  W_fc2 = weight_variable([layer4, 10], True)
-  y_conv = tf.matmul(A4, W_fc2)
+  with tf.name_scope('layer5'):
+      W_fc2 = weight_variable([layer4, 10])
+      y_conv = tf.matmul(A4, W_fc2)
 
   return y_conv, keep_prob
 
-def weight_variable(shape, final):
-  """weight_variable generates a weight variable of a given shape with given sparsity"""
-  """weight prunning"""
-  #initialize weight
-  initial = tf.truncated_normal(shape, stddev=0.1)
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
-  if final:
-      return tf.Variable(initial)
+def weight_pruning(sess, sparsity):
+  """weight pruning"""
+  #get all trained variables
+  all_variables = tf.global_variables()
+  print(all_variables)
+  all_values = sess.run(all_variables)
 
-  #get numpy arrya from Tensor
-  init = tf.initialize_all_variables()
-  sess = tf.InteractiveSession()
-  sess.run(init)
-  sess.run(tf.global_variables_initializer())
-  matrix = initial.eval(session=sess)
+  for var, val in zip(all_variables, all_values):
+      if "layer5" not in var.name:
+          matrix = np.array(val)
+          if matrix.shape != ():
 
-  #sizes
-  x = matrix.shape[0]
-  y = matrix.shape[1]
-  size = x * y
+              #sizes
+              x = matrix.shape[0]
+              y = matrix.shape[1]
+              size = x * y
 
-  #find magnitude of matrix and indexes of SPARCITY% lowest values
-  magnitude_matrix = np.absolute(matrix)
-  magnitude_matrix = np.reshape(magnitude_matrix, [size])
-  number_of_valuest = int((size * SPARSITY) / 100)
-  idx = np.argpartition(magnitude_matrix, number_of_valuest)
+              #find magnitude of matrix and indexes of SPARCITY% lowest values
+              magnitude_matrix = np.absolute(matrix)
+              magnitude_matrix = np.reshape(magnitude_matrix, [size])
+              number_of_valuest = int((size * sparsity) / 100)
+              idx = np.argpartition(magnitude_matrix, number_of_valuest)
 
-  # set lowest to zero and return weight matrix
-  matrix = np.reshape(matrix, [size])
-  matrix[idx[0:number_of_valuest]] = 0
-  initial = tf.reshape(matrix, [x, y])
-  return tf.Variable(initial)
+              # set lowest to zero and return weight matrix
+              matrix = np.reshape(matrix, [size])
+              matrix[idx[0:number_of_valuest]] = 0
 
-def l2_weight_variable(shape, final):
-  """weight_variable generates a weight variable of a given shape with given sparsity"""
-  """weight prunning"""
-  #initialize weight
-  initial = tf.truncated_normal(shape, stddev=0.1)
+              initial = tf.reshape(matrix, [x, y])
+              tf.assign(var, initial)
 
-  if final:
-      return tf.Variable(initial)
+def l2_pruning(sess, sparsity):
+  """l2 norm prunning"""
 
-  #get numpy arrya from Tensor
-  init = tf.initialize_all_variables()
-  sess = tf.InteractiveSession()
-  sess.run(init)
-  sess.run(tf.global_variables_initializer())
-  matrix = initial.eval(session=sess)
+  # get all trained variables
+  all_variables = tf.global_variables()
+  all_values = sess.run(all_variables)
 
-  #sizes
-  x = matrix.shape[0]
-  y = matrix.shape[1]
+  for var, val in zip(all_variables, all_values):
+      if "layer5" not in var.name:
+          matrix = np.array(val)
+          if matrix.shape != ():
 
-  #find indeces of columns with smallest L2-norm value
-  i = 0
-  all_norms = []
-  while i < y:
-      all_norms.append(np.linalg.norm(matrix[:, i], ord=2))
-      i += 1
-  number_of_values = int((y * SPARSITY) / 100)
-  idx = np.argpartition(all_norms, number_of_values)
+              #sizes
+              x = matrix.shape[0]
+              y = matrix.shape[1]
 
-  #set columns with smallest l2-norm to zero
-  for index in idx[0:number_of_values]:
-      matrix[:,index] = 0
+              #find indeces of columns with smallest L2-norm value
+              i = 0
+              all_norms = []
+              while i < y:
+                  all_norms.append(np.linalg.norm(matrix[:, i], ord=2))
+                  i += 1
+              number_of_values = int((y * sparsity) / 100)
+              idx = np.argpartition(all_norms, number_of_values)
 
-  initial = tf.reshape(matrix, [x, y])
-  return tf.Variable(initial)
+              #set columns with smallest l2-norm to zero
+              for index in idx[0:number_of_values]:
+                  matrix[:,index] = 0
 
+              initial = tf.reshape(matrix, [x, y])
+              tf.assign(var, initial)
+
+def get_mean(batch):
+    i = 0
+    probs = []
+    while i < len(batch):
+        probs.append(max(batch[i]))
+        i += 1
+    return np.mean(probs)
 
 # training on mnist dataset when run first time
-def train(sess, mnist, accuracy, keep_prob, train_step, x, y_):
+def train(mnist, accuracy, keep_prob, train_step, x, y_):
     for i in range(20000):
         batch = mnist.train.next_batch(250)
         if i % 100 == 0:
@@ -159,12 +168,25 @@ def train(sess, mnist, accuracy, keep_prob, train_step, x, y_):
 
 
 # create adversarial and reevaluate with model
-def eval(accuracy, mnist, keep_prob, x, y_):
-    eval_data = mnist.test.images
-    eval_labels = mnist.test.labels
-    eval_accuracy = accuracy.eval(feed_dict={x: eval_data, y_: eval_labels, keep_prob: 1.0})
+def eval(sess, mnist, keep_prob, x, y_conv, y_, pruning):
+    final = []
+    sparsities = [0, 25, 50, 60, 70, 80, 90, 95, 97, 99]
 
-    return eval_accuracy
+    for sparsity in sparsities:
+
+        if pruning == "weight":
+            weight_pruning(sess, sparsity)
+        if pruning == "l2":
+            l2_pruning(sess, sparsity)
+
+        y = tf.nn.softmax(y_conv)
+        eval_data = mnist.test.images
+        eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+        prob = y.eval(feed_dict={x: eval_data, y_: eval_labels, keep_prob: 1.0})
+
+        final.append((sparsity, prob))
+
+    return final
 
 
 def main(_):
@@ -197,13 +219,20 @@ def main(_):
   train_writer.add_graph(tf.get_default_graph())
 
   # creating new session
-  init = tf.initialize_all_variables()
-  sess1 = tf.InteractiveSession()
-  sess1.run(init)
-  sess1.run(tf.global_variables_initializer())
+  sess = tf.InteractiveSession()
+  sess.run(tf.global_variables_initializer())
 
-  #train(sess1, mnist, accuracy, keep_prob, train_step, x, y_)
-  print(eval(accuracy, mnist, keep_prob, x, y_))
+  #train(mnist, accuracy, keep_prob, train_step, x, y_)
+
+  all_variables = tf.global_variables()
+  print(len(all_variables))
+
+  pruning = 'weight'
+  #values = eval(sess, mnist, keep_prob, x, y_conv, y_, pruning)
+  #for value in values:
+  #    get_mean(value[1])
+
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
